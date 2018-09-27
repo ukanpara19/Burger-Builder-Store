407451616ad140547a3deb13f58e6c54e436bb2c
utsav <ukanpara@scu.edu>
2018-09-27 12:51:27 -0700

    fix : Updated new title logo

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,23 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
-
-
-// ENZYME
-// import React from 'react'
-// import { shallow } from 'enzyme'
-// import App from './App'
-
-// it('renders without crashing', () => {
-//   shallow(<App />)
-// })
-
-
-
-// shallow — renderuje tylko ten komponent (bez komponentów w nim zagnieżdżonych).
-// Idealny do testów jednostkowych, bo masz pewność, że przypadkiem nie sprawdzasz zachowań innych komponentów w środku. Szybki.
-
-// mount — komponent jest renderowany i montowany w DOM.
-// Do testowania komponentów, które wchodzą w interakcje z DOM albo są udekorowane przez High Order Components
-
-// render — renderuje komponent do statycznego HTML-a
